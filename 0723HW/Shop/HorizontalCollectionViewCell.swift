90bcb7e4cd2e58bf1da338ea28a34bab8873010e
Jimin Seo <tig05015@naver.com>
2025-07-29 22:51:09 +0900

    @available(*, unavailable) 추가

--- 0723HW/Shop/HorizontalCollectionViewCell.swift
+++ 0723HW/Shop/HorizontalCollectionViewCell.swift
@@ -26,6 +26,7 @@ class HorizontalCollectionViewCell: UICollectionViewCell {
         configureLayout()
     }
     
+    @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
--- 0723HW/Shop/ShopCollectionViewCell.swift
+++ 0723HW/Shop/ShopCollectionViewCell.swift
@@ -58,6 +58,7 @@ class ShopCollectionViewCell: UICollectionViewCell {
         configureLayout()
     }
     
+    @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
